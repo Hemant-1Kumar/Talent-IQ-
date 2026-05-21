b61745f6c28f22fb383a00bb0ba5d8c7a6bd7c6c
Hemant-1Kumar <hemantkumarr831@gmail.com>
2026-05-21 14:57:41 +0530

    Added frontend dist build

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,30 +17,31 @@ const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware());
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-// routes
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-// health check
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
 
-// --------------------
-// PRODUCTION FRONTEND
-// --------------------
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// start server
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log("Server is running on port:", ENV.PORT)
-    );
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
